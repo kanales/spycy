a412b503302355475ab61d02fc65d211ea82b98e
kanales <ivancanalestgna@gmail.com>
2017-07-11 23:38:04 +0200

    Initial commit for the package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup( name='spycy'
+     , version='0.2'
+     , description='Small library for auto-currying functions and improved functional application'
+     , url='http://github.com/kanales/spycy'
+     , author='Ivan Canales'
+     , author_email='kanales.contact@gmail.com'
+     , license='MIT'
+     , packages=['spycy']
+     , zip_safe=False
+     )
